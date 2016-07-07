4bcdc91150cba1efdfbc2c2ae2b3300cc0491200
kenneth2016 <kennethl1801@gmail.com>
2016-07-07 09:19:09 -0600

    codigo empezado totito

---
+++ totito.py
@@ -0,0 +1,45 @@
+
+print ("bienvenido al totito supremo 2016 69")
+l= [["a", "b", "c"],
+	["d", "e", "f"],
+	["g", "h", "i"],
+	]
+
+print (l[0])
+print (l[1])
+print (l[2])
+
+p= str(input("ingrese que poscicion quiere: "))
+
+if p=="a":
+	l[0][0]="x"
+
+if p=="b":
+	l[0][1]="x"
+
+if p=="c":
+	l[0][2]="x"
+
+if p=="d":
+	l[1][0]="x"
+
+if p=="e":
+	l[1][1]="x"
+
+if p=="f":
+	l[1][2]="x"
+
+if p=="g":
+	l[2][0]="x"
+
+if p=="h":
+	l[2][1]="x"
+
+if p=="i":
+	l[2][2]="x"
+
+
+
+print (l[0])
+print (l[1])
+print (l[2])
